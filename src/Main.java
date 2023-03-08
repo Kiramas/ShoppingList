c4b32146a6631fc283b45af72bd202d2497aaff9
Kira Kolmakova <lyraoctavia@gmail.com>
2023-03-08 15:43:28 +0500

    extract method

--- src/Main.java
+++ src/Main.java
@@ -12,46 +12,46 @@ public class Main {
         while (true) {
             System.out.print("Выберите операцию: ");
             String line = scanner.nextLine();
-            try {
-                int value = Integer.parseInt(line);
-            } catch (NumberFormatException e) {
-            } finally {
-                int value = Integer.parseInt(line);
-                if (value == 1) {
-                    System.out.print("Какую покупку хотите добавить?");
-                    String product = scanner.nextLine();
-                    list.add(product);
-                    System.out.print("Итого в списке покупок: " + list.size());
-                }
-                if (value == 2) {
-                    System.out.println("Список покупок: ");
-                    for (int i = 0; i < list.size(); i++) {
-                        System.out.println(i + 1 + ". " + list.get(i));
+            int value = Integer.parseInt(line);
+            if (value == 1) {
+                addProduct(list, scanner);
+            }
+            if (value == 2) {
+                searchProduct(list);
+            }
+            if (value == 3) {
+                searchProduct(list);
+                System.out.println("Какую покупку хотите удалить? Введите номер или название");
+                String productDelete = scanner.nextLine();
+                try {
+                    int productDeleteInt = Integer.parseInt(productDelete);
+                    productDelete = list.get(productDeleteInt - 1);
+                    list.remove(productDeleteInt - 1);
+                } catch (NumberFormatException e) {
+                    if (list.contains(productDelete)) {
+                        list.remove(productDelete);
                     }
                 }
-                if (value == 3) {
-                    System.out.println("Список покупок: ");
-                    for (int i = 0; i < list.size(); i++) {
-                        System.out.println(i + 1 + ". " + list.get(i));
-                    }
-                    System.out.println("Какую покупку хотите удалить? Введите номер или название");
-                    String productDelete = scanner.nextLine();
-                    try {
-                        int productDeleteInt = Integer.parseInt(productDelete);
-                        productDelete = list.get(productDeleteInt - 1);
-                        list.remove(productDeleteInt - 1);
-                    } catch (NumberFormatException e) {
-                        if (list.contains(productDelete)) {
-                            list.remove(productDelete);
-                        }
-                    }
-                    System.out.println("Покупка " + productDelete + " удалена из вашего списка");
-                    for (int i = 0; i < list.size(); i++) {
-                        System.out.println((i + 1) + ". " + list.get(i));
-                    }
+                System.out.println("Покупка " + productDelete + " удалена из вашего списка");
+                for (int i = 0; i < list.size(); i++) {
+                    System.out.println((i + 1) + ". " + list.get(i));
                 }
             }
         }
+    }
+
+    private static void searchProduct(ArrayList<String> list) {
+        System.out.println("Список покупок: ");
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println(i + 1 + ". " + list.get(i));
+        }
+    }
+
+    private static void addProduct(ArrayList<String> list, Scanner scanner) {
+        System.out.print("Какую покупку хотите добавить?");
+        String product = scanner.nextLine();
+        list.add(product);
+        System.out.print("Итого в списке покупок: " + list.size());
     }
 }
 
